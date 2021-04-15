ac5dcd72566158ded7af2ace4d5963b381768ebe
SARA RUSSERT <sarabeth.russert@gmail.com>
2021-04-15 11:53:10 -0700

    adds class-09 demo

--- class-09/demo/js/app.js
+++ class-09/demo/js/app.js
@@ -1,5 +1,12 @@
 // problem domain: the Seattle Kitten Rescue has tons of kittens who need good homes. One of the best ways to reach prospective adoptive homes is to have profiles for each kitten available on a website. There are hundreds of kittens, though, and only a few volunteers; it's too time-consuming to hand-code each kitten's profile on their website. They need a better way.
+// jose got a real job, no longer volunteering. In his last week wants to make sure other volunteers can add cats
 
+// global variables
+const formElem = document.getElementById('addKittenForm')
+// add a listener!
+
+
+// constructor functions
 // create a kitten factory aka constructor function
 function Kitten(name, photo, interests, isGoodWithKids, isGoodWithDogs, isGoodWithCats) {
   this.name = name;
@@ -10,11 +17,8 @@ function Kitten(name, photo, interests, isGoodWithKids, isGoodWithDogs, isGoodWi
   this.isGoodWithDogs = isGoodWithDogs;
 }
 
-// global function that gives us a random age 
-function randomAge (min, max) {
-  return Math.floor(Math.random() * (max - min + 1) + min);
-}
 
+// methods - prototypes
 // add a get age prototype method
 Kitten.prototype.getAge = function() {
   this.age = randomAge(3, 12) + ' months'
@@ -86,6 +90,48 @@ Kitten.prototype.render = function() {
 
 }
 
+// regular functions
+// global function that gives us a random age 
+function randomAge (min, max) {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function handleSubmit(event) {
+  // for form events
+  event.preventDefault();
+  console.log(event.target);
+  console.log(event.target.interests.value)
+
+  // event "what happened"
+  // target the form
+  // photo = the name from the input field we want info from
+  // value = what the user put in that field
+  // what are we going to do with all this info?
+
+  let name = event.target.name.value;
+  let photo = event.target.photo.value;
+  let interests = event.target.interests.value.split(',');
+  // split is a method on strings
+  // takes a string and turns it into an array
+  console.log(interests);
+  let isGoodWithKids = event.target.isGoodWithKids.checked;
+  // gives us back true or false if it is checked
+  console.log(isGoodWithKids);
+  let isGoodWithDogs = event.target.isGoodWithDogs.checked;
+  let isGoodWithOtherCats = event.target.isGoodWithCats.checked;
+  // catArray.push(new Kitten(name, photo, interests, isGoodWithKids, isGoodWithDogs, isGoodWithOtherCats))
+  let newCatIMadeFromTheForm = new Kitten(name, photo, interests, isGoodWithKids, isGoodWithDogs, isGoodWithOtherCats);
+  newCatIMadeFromTheForm.getAge();
+  newCatIMadeFromTheForm.render();
+
+  // we do this event and a cat is rendered - this variable is scoped to the form and when this function ends in some sense this variable ceases to be 
+
+  // wipe out form
+  event.target.reset();
+}
+
+// declare the cats after the function because I want to
+
 let frankie = new Kitten('Frankie', './images/frankie.jpeg', ['lazers', 'mice', 'string'], true, false, true);
 
 
@@ -93,7 +139,26 @@ const jumper = new Kitten('jumper', './images/jumper.jpeg', ['sunbeams', 'yarn',
 
 const serena = new Kitten('serena', './images/serena.jpeg', ['sitting on laps', 'climbing curtains', 'eating treats'], true, null, true);
 
+// what element are we listening to?
+// form - addKittenForm
+// what are we listening for?
+// submit
 
+
+// listener
+
+formElem.addEventListener('submit', handleSubmit)
+// when the submit happens on the FORM call the handleSubmit function
+
+// what the addEventListener method looks like behind the scenes:
+// function addEventListener(whatToListenFor, callbackFunction) {
+//   if (whatToListenFor happens) {
+//     // callBackFunction(event)
+//   }
+  
+// }
+
+// call functions
 
 frankie.getAge();
 jumper.getAge();
